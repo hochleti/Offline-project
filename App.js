39537abfdf56830af3a531e82d623c84c5cd2a6f
Kevin <kevinchua88@yahoo.com.sg>
2021-10-15 02:57:02 +1000

    database

--- App.js
+++ App.js
@@ -1,8 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, Animated, Component } from 'react-native';
+import { StyleSheet, Text, View, Button, Animated, Component, TextInput } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import CountDown from 'react-native-countdown-component';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set, postsRef } from "firebase/database";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHsQgEqfF04HPbUQU0C8jQ5mYcFZ7NODk",
+  authDomain: "offline-ea045.firebaseapp.com",
+  databaseURL: "https://offline-ea045-default-rtdb.firebaseio.com",
+  projectId: "offline-ea045",
+  storageBucket: "offline-ea045.appspot.com",
+  messagingSenderId: "383467579431",
+  appId: "1:383467579431:web:ab37fc8dd6776d5d374602",
+  measurementId: "G-7YXYN7DE4Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
+function writeUserData(username, timeSpent) {
+  set(ref(database, 'users/' + username), {
+    time: timeSpent
+  });
+  alert('Finished')
+}
 
 export default function App() {
   const [date, setDate] = useState(new Date(0, 0));
@@ -18,6 +45,8 @@ export default function App() {
     setMinute(currentDate.getMinutes());
   };
 
+
+  
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
       <View >
@@ -25,7 +54,7 @@ export default function App() {
       <CountDown
         size={30}
         until={date.getHours()*3600 + date.getMinutes()*60}
-        onFinish={() => alert('Finished')}
+        onFinish={() => writeUserData("test",1)}
         digitStyle={{backgroundColor: '#FFF', borderWidth: 2, borderColor: '#1CC625'}}
         digitTxtStyle={{color: '#1CC625'}}
         timeLabelStyle={{color: 'red', fontWeight: 'bold'}}
