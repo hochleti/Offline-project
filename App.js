1275b9974b1aecdde4affd7ebccc8f13e1139c68
hochleti <hochauleticia25@gmail.com>
2021-10-14 11:26:42 +1000

    Added a working timer

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, Animated, Component } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+import CountDown from 'react-native-countdown-component';
 
 export default function App() {
   const [date, setDate] = useState(new Date(0, 0));
   const [show, setShow] = useState(false);
   const [hour, setHour] = useState(0);
   const [minute, setMinute] = useState(0);
+  const [shouldShow, setShouldShow] = useState(false);
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
@@ -16,89 +17,37 @@ export default function App() {
     setHour(currentDate.getHours());
     setMinute(currentDate.getMinutes());
   };
-  const minuteSeconds = 60;
-  const hourSeconds = 3600;
-  const daySeconds = 86400;
-
-  const timerProps = {
-    isPlaying: true,
-    size: 120,
-    strokeWidth: 6
-  };
-  const renderTime = (dimension, time) => {
-    return (
-      <View>
-        <Animated.Text>{time}</Animated.Text>
-        <Animated.Text>{dimension}</Animated.Text>
-      </View>
-    );
-
-  };
-   const getTimeSeconds = (time) => (minuteSeconds - time) | 0;
-   const getTimeMinutes = (time) => ((time % hourSeconds) / minuteSeconds) | 0;
-  const getTimeHours = (time) => ((time % daySeconds) / hourSeconds) | 0;
-  const getTimeDays = (time) => (time / daySeconds) | 0;
-
-  const startTime = date.getTime() / 1000; // use UNIX timestamp in seconds
-  const endTime = startTime + 243248; // use UNIX timestamp in seconds
-
-  const remainingTime = endTime - startTime;
-  const days = Math.ceil(remainingTime / daySeconds);
-  const daysDuration = days * daySeconds;
-
-
 
   return (
-
     <View style={{ flex: 1, justifyContent: "center" }}>
+      <View >
+      {shouldShow ? (
+      <CountDown
+        size={30}
+        until={date.getHours()*3600 + date.getMinutes()*60}
+        onFinish={() => alert('Finished')}
+        digitStyle={{backgroundColor: '#FFF', borderWidth: 2, borderColor: '#1CC625'}}
+        digitTxtStyle={{color: '#1CC625'}}
+        timeLabelStyle={{color: 'red', fontWeight: 'bold'}}
+        separatorStyle={{color: '#1CC625'}}
+        timeToShow={['H', 'M', 'S']}
+        timeLabels={{h: 'Hours', m: 'Minutes', s: 'Seconds'}}
+        showSeparator
+      />
+      ) : null}
+      </View>
+      <View>
+      
       <DateTimePicker
         value={date}
         mode={"countdown"}
         is24Hour={true}
         onChange={onChange}
       />
-      <View>
-        <Button title="Start/Stop" />
       </View>
-      <CountdownCircleTimer
-        {...timerProps}
-        colors={[["#D14081"]]}
-        duration={daySeconds}
-        initialRemainingTime={remainingTime % daySeconds}
-        onComplete={(totalElapsedTime) => [
-          remainingTime - totalElapsedTime > hourSeconds
-        ]}
-      >
-        {({ elapsedTime }) =>
-          renderTime("hours", getTimeHours(daySeconds - elapsedTime  ))
-        }
-      </CountdownCircleTimer>
-      <CountdownCircleTimer
-        {...timerProps}
-        colors={[["#EF798A"]]}
-        duration={hourSeconds}
-        initialRemainingTime={remainingTime % hourSeconds}
-        onComplete={(totalElapsedTime) => [
-          remainingTime - totalElapsedTime > minuteSeconds
-        ]}
-      >
-        {({ elapsedTime }) =>
-          renderTime("minutes", getTimeMinutes(hourSeconds - elapsedTime))
-        }
-      </CountdownCircleTimer>
-      <CountdownCircleTimer
-        {...timerProps}
-        colors={[["#218380"]]}
-        duration={minuteSeconds}
-        initialRemainingTime={remainingTime % minuteSeconds}
-        onComplete={(totalElapsedTime) => [
-          remainingTime - totalElapsedTime > 0
-        ]}
-      >
-        {({ elapsedTime }) =>
-          renderTime("seconds", getTimeSeconds(elapsedTime))
-        }
-      </CountdownCircleTimer>
+      <View>
+        <Button title="Start/Stop" onPress={() => setShouldShow(!shouldShow)} />
+      </View>
     </View>
   );
 }
