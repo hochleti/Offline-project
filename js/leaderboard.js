16fecd0aa222a26cc1dfcb32f18411b5624ce211
hochleti <hochauleticia25@gmail.com>
2021-10-21 01:45:56 +1000

    Update leaderboard.js

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,33 +1,61 @@
 import * as React from 'react';
 import { useState } from "react";
-import { StyleSheet, Text, View, Animated, Component, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Animated, Component, Button, TextInput, FlatList, } from 'react-native';
 import Firebase from "./firebase";
 
 export function LeaderBoard() {
-    
-    var myList = new Array();
-    function getList() {
-        return myList;
-    }
-    Firebase.database().ref('users').orderByChild("time").limitToLast(5).once('value', function(snapshot) {
-        snapshot.forEach(function(child) {
-            
-            const test = child.key;
-            const test2 = child.val().time;
-            //console.log(test + " " + test2) // NOW THE CHILDREN PRINT IN ORDER
-            const data = [child.key, child.val().time];
-            console.log(data)
-            myList.push(data);
+    const [items, setItems] = useState([]);
+
+    Firebase.database().ref('users').orderByChild("time").limitToLast(5).once('value').then(function (snapshot) {
+        snapshot.forEach(function (child) {
+            if (items.length < 6) {
+
+                setItems([...items, {
+                    name: child.key,
+                    time: child.val().time
+                }])
+            }
         });
-        const testList = getList();
-        console.log("new list " + getList());
-      });
-    console.log("NEW LIST " + getList);
+    });
+
+    const styles = StyleSheet.create({
+        text: {
+            fontFamily: "Charter",
+            fontSize: 20,
+            color: "white",
+            textAlign: "center",
+            paddingRight: 100,
+            paddingBottom: 15,
+        },
+        title: {
+            paddingTop: 15,
+            textAlign: "center",
+            fontSize: 25,
+            color: "white",
+            fontFamily: "Charter",
+            paddingBottom: 30,
+        },
+        subtitle:{
+            fontFamily: "Charter",
+            fontSize: 20,
+            color: "white",
+            textAlign: "center",
+            paddingBottom: 15,
+        },
+    });
     return (
-        /*Start writing here*/
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Leaderboard</Text>
+        <View style={{ flex: 1, paddingTop: 100, backgroundColor: '#89C7D6' }}>
+            <Text style={styles.title}>Leaderboard</Text>
+            <View flexDirection="col">
+                <Text style={styles.subtitle}>Team   Time Completed (Minutes)</Text>
+                {items.map(item => (
+                    <View flexDirection="row" style={{ marginLeft: "auto",  marginRight: "auto" }}>
+                        <Text style={styles.text}>{item.name}</Text>
+                        <Text style={styles.text}>{item.time}</Text>
+                    </View>
+                ))}
+            </View>
         </View>
     );
 
-}
+};
