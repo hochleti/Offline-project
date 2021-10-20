35071e81feceaa7606238bf87fa05cb9145aa054
sytosis <69778127+sytosis@users.noreply.github.com>
2021-10-20 20:32:03 +1000

    leaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -4,11 +4,25 @@ import { StyleSheet, Text, View, Animated, Component, Button, TextInput } from '
 import Firebase from "./firebase";
 
 export function LeaderBoard() {
+    
+    var myList = new Array();
+    function getList() {
+        return myList;
+    }
     Firebase.database().ref('users').orderByChild("time").limitToLast(5).once('value', function(snapshot) {
         snapshot.forEach(function(child) {
-            console.log(child.key + " " + child.val().time) // NOW THE CHILDREN PRINT IN ORDER
+            
+            const test = child.key;
+            const test2 = child.val().time;
+            //console.log(test + " " + test2) // NOW THE CHILDREN PRINT IN ORDER
+            const data = [child.key, child.val().time];
+            console.log(data)
+            myList.push(data);
         });
+        const testList = getList();
+        console.log("new list " + getList());
       });
+    console.log("NEW LIST " + getList);
     return (
         /*Start writing here*/
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
