b8ff1115715c39d405f12327dcd92f6ed121daf1
Anna Chaourova <a.chaourova@uqconnect.edu.au>
2021-10-20 17:04:11 +1000

    fixed links for articles

--- js/learn_more_page.js
+++ js/learn_more_page.js
@@ -1,25 +1,29 @@
 
 import * as React from 'react';
-import { View, Text, StyleSheet, Pressable, Image} from 'react-native';
+import { View, Text, StyleSheet, Pressable, Image, Linking} from 'react-native';
 import exampleImage from '../assets/learning.png';
 
 export function LearnMorePage() {
 
     // read an article
     function articleClick() {
-        window.open("https://www.sciencedirect.com/science/article/abs/pii/S0747563215303162");
+        Linking.openURL('https://www.sciencedirect.com/science/article/abs/pii/S0747563215303162');
+        //window.open("https://www.sciencedirect.com/science/article/abs/pii/S0747563215303162");
     }
     // google results
     function googleClick() {
-        window.open("https://www.google.com/search?q=what+is+smartphone+addiction&oq=what+is+&aqs=chrome.0.69i59j69i57j0i512l2j0i131i433i512j0i433i512j0i512j69i60.2122j0j7&sourceid=chrome&ie=UTF-82");
+        Linking.openURL('https://www.google.com/search?q=what+is+smartphone+addiction&oq=what+is+&aqs=chrome.0.69i59j69i57j0i512l2j0i131i433i512j0i433i512j0i512j69i60.2122j0j7&sourceid=chrome&ie=UTF-82');
+        //window.open("https://www.google.com/search?q=what+is+smartphone+addiction&oq=what+is+&aqs=chrome.0.69i59j69i57j0i512l2j0i131i433i512j0i433i512j0i512j69i60.2122j0j7&sourceid=chrome&ie=UTF-82");
     }
     // speak to professional
     function professionalClick() {
-        window.open("https://www.qld.gov.au/health/mental-health/help-lines/services#:~:text=Health%20professionals&text=Phone%20the%20mental%20health%20access,to%20public%20mental%20health%20services.");
+        Linking.openURL('https://www.qld.gov.au/health/mental-health/help-lines/services#:~:text=Health%20professionals&text=Phone%20the%20mental%20health%20access,to%20public%20mental%20health%20services.');
+        //window.open("https://www.qld.gov.au/health/mental-health/help-lines/services#:~:text=Health%20professionals&text=Phone%20the%20mental%20health%20access,to%20public%20mental%20health%20services.");
     }
     // call hotline
     function hotlineClick() {
-        window.open("https://www.beyondblue.org.au/the-facts/suicide-prevention/get-support-now?gclid=EAIaIQobChMI4drkzuTT8wIVwyMrCh0aXArdEAAYASAAEgIa7PD_BwE");
+        Linking.openURL('https://www.beyondblue.org.au/the-facts/suicide-prevention/get-support-now?gclid=EAIaIQobChMI4drkzuTT8wIVwyMrCh0aXArdEAAYASAAEgIa7PD_BwE');
+        //window.open("https://www.beyondblue.org.au/the-facts/suicide-prevention/get-support-now?gclid=EAIaIQobChMI4drkzuTT8wIVwyMrCh0aXArdEAAYASAAEgIa7PD_BwE");
     }
 
     const styles = StyleSheet.create({
